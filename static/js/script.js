c0a9c6128def84c2f11b95c3fd6ad72a68dbfbf7
Garfun-test <garfun1@gmail.com>
2021-03-28 13:39:00 +0300

    forgotten push front

--- static/js/script.js
+++ static/js/script.js
@@ -17,17 +17,12 @@ function getLike(){
         method: 'GET',
         success: function (data){
             $("#likes-count").html(data['likes']);
-            // showLike(data['likes']);
         }
     });
 }
 
 function showFormComment(){
-    $("#form-comment").removeClass("d-none");
-//    console.log("#form-comment")
-//    setTimeout(() => {
-//        $("#form-comment").addClass("d-none");
-//    }, 5000);
+    $("#form-comment").toggleClass("d-none");
 }
 
 
@@ -42,37 +37,40 @@ function sendComment(comment) {
       $("#form-comment")[0].reset();
       $("#form-comment").addClass("d-none")
       alert("Comment has been added")
+      loadComments()
     }
   })
 }
 
-//function showComments(comments) {
-//  $(".comments-list").html("")
-//  comments.forEach((comment) => {
-//    const el = $("<div class='pt-2'></div>").html(`
-//    <hr /><b>${comment.author}</b>: ${comment.body}
-//    `);
-//    $(".comments-list").append(el)
-//  })
-//
-//}
+function loadComments() {
+  const postId = $("#post-id").val();
+  $(".comments-list").html("Loading...")
+  $.ajax({
+    url: `/api/article/posts/${postId}/comments/`,
+    method: "GET",
+    success: function(data) {
+        showComments(data);
+    }
+  })
+}
 
-//function loadComments() {
-//  const postId = $("#post-id").val();
-//  $(".comments-list").html("Loading...")
-//  $.ajax({
-//    url: `/api/article/posts/${postId}/comments/`,
-//    method: "GET",
-//    success: function(data) {
-//        console.log(data.results)
-//      showComments(data.results);
-//    }
-//  })
-//
-//}
+function showComments(comments) {
+  $(".comments-list").html("")
+  comments.forEach((comment) => {
+    const el = $("<div class='pt-2'></div>").html(`
+    <p>
+        <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-person-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+          <path fill-rule="evenodd" d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"/>
+        </svg>
+        <b>${ comment.author }:</b> ${ comment.body }</p>
+      <hr>
+    `);
+    $(".comments-list").append(el)
+  })
+}
 
 $(document).ready(function() {
-  //loadComments();
+  loadComments();
 
   $('#do_like').click(function(e){
       putLike();
